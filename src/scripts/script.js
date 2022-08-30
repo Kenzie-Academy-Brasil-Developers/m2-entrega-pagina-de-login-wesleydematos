f1a4631f1a662ec704b39f315300172ef09f5304
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-08-29 21:47:20 -0300

    fix: limpa input

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -4,9 +4,11 @@ let btnModal = document.getElementById('btnModal')
 let spanFechaPopUp = document.getElementById('fechaPopUp')
 let modalSenha = document.querySelector('.section__modal__senha')
 let popUp = document.querySelector('.section__popup')
+let inputEmail = document.getElementById('email')
 
 function abreFechaModal(span){
     span.addEventListener('click', ()=>{
+        inputEmail.value = ''
         modalSenha.classList.toggle('hidden')
     })
 }
@@ -14,10 +16,10 @@ abreFechaModal(spanSenhaEsquecida)
 abreFechaModal(spanFechaModal)
 
 function abrePopUp(){
-    let inputEmail = document.getElementById('email')
     btnModal.addEventListener('click', ()=>{
         if(inputEmail.value.includes('@')){
             modalSenha.classList.toggle('hidden')
+            inputEmail.value = ''
             popUp.classList.toggle('hidden')
         }else{
             alert('Insira um email válido!')
@@ -28,6 +30,7 @@ abrePopUp()
 
 function fechaPopUp(){
     spanFechaPopUp.addEventListener('click', ()=>{
+        inputEmail.value = ''
         popUp.classList.toggle('hidden')
     })
 }
